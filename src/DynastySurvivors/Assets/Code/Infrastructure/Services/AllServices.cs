deb8404888a9e6ccbf508ee52c162aa8def1d61e
Olsney <104034829+Olsney@users.noreply.github.com>
2025-04-13 22:29:26 +0300

    Reworked AllServices logic

--- src/DynastySurvivors/Assets/Code/Infrastructure/Services/AllServices.cs
+++ src/DynastySurvivors/Assets/Code/Infrastructure/Services/AllServices.cs
@@ -1,21 +1,41 @@
-﻿using Code.Infrastructure.Factory;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Code.Infrastructure.Services
 {
     public class AllServices
     {
         private static AllServices _instance;
-        public static AllServices Container => _instance  ?? (_instance = new AllServices());
+        public static AllServices Container => _instance ?? (_instance = new AllServices());
+        private Dictionary<Type, IService> _services;
 
-        public void RegisterSingle<TService>(TService implementation) where TService : IService => 
-            Implementation<TService>.ServiceInstance = implementation;
-
-        public TService Single<TService>() where TService : IService => 
-            Implementation<TService>.ServiceInstance;
-
-        private static class Implementation<TService> where TService : IService
+        public AllServices()
         {
-            public static TService ServiceInstance;
+            _services = new Dictionary<Type, IService>();
         }
+
+        public void RegisterSingle<TService>(TService implementation) where TService : IService
+        {
+            _services[typeof(TService)] = implementation;
+        }
+        
+        public TService Single<TService>() where TService : class, IService
+        {
+            if (_services.ContainsKey(typeof(TService)) == false)
+                throw new KeyNotFoundException();
+            
+            return _services[typeof(TService)] as TService;
+        }
+
+        // public void RegisterSingle<TService>(TService implementation) where TService : IService => 
+        //     Implementation<TService>.ServiceInstance = implementation;
+        //
+        // public TService Single<TService>() where TService : IService => 
+        //     Implementation<TService>.ServiceInstance;
+        //
+        // private static class Implementation<TService> where TService : IService
+        // {
+        //     public static TService ServiceInstance; 
+        // }
     }
 }

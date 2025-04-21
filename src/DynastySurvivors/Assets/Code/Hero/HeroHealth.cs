747a452c7d8bc80f243554ecfc3d1cd7595ac872
Olsney <104034829+Olsney@users.noreply.github.com>
2025-04-21 21:41:35 +0300

    Added hero health

---
+++ src/DynastySurvivors/Assets/Code/Hero/HeroHealth.cs
@@ -0,0 +1,44 @@
+﻿using Code.Data;
+using Code.Services.PersistentProgress;
+using UnityEngine;
+
+namespace Code.Hero
+{
+    public class HeroHealth : MonoBehaviour, ISavedProgress
+    {
+        [SerializeField] private HeroAnimator _animator;
+        private State _state;
+
+        public float Current
+        {
+            get => _state.CurrentHp;
+            set => _state.CurrentHp = value;
+        }
+
+        public float Max
+        {
+            get => _state.MaxHp;
+            set => _state.MaxHp = value;
+        }
+
+        public void LoadProgress(PlayerProgress progress)
+        {
+            _state = progress.HeroState;
+        }
+
+        public void UpdateProgress(PlayerProgress progress)
+        {
+            progress.HeroState.CurrentHp = Current;
+            progress.HeroState.MaxHp = Max;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            if (Current <= 0)
+                return;
+
+            Current -= damage;
+            _animator.PlayHit();
+        }
+    }
+}

6a1ce383995999714b8d8a8f28bbb9fda4fe8cd0
Olsney <104034829+Olsney@users.noreply.github.com>
2025-04-29 00:45:44 +0300

    Added hero death vfx

--- src/DynastySurvivors/Assets/Code/Hero/HeroDeath.cs
+++ src/DynastySurvivors/Assets/Code/Hero/HeroDeath.cs
@@ -13,7 +13,9 @@ namespace Code.Hero
         [SerializeField]
         private HeroAnimator _animator;
         [SerializeField]
-        private GameObject _deathEffect;
+        private GameObject _deathEffectPrefab;
+        [SerializeField]
+        private Transform _deathEffectSpawnPoint;
 
         private bool _isDead;
 
@@ -31,11 +33,14 @@ namespace Code.Hero
 
         private void Die()
         {
+            float effectVisualDiration = 3f;
+            
             _move.enabled = false;
             _animator.PlayDeath();
-            Instantiate(_deathEffect, transform.position, Quaternion.identity);
-
+            GameObject deathEffect = Instantiate(_deathEffectPrefab, _deathEffectSpawnPoint.position, Quaternion.identity);
             _isDead = true;
+            
+            Destroy(deathEffect, effectVisualDiration);
         }
     }
 }

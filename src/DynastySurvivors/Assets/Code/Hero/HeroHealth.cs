a03a0c94a45838d309ba2c1f4f4be9784446b1ac
Olsney <104034829+Olsney@users.noreply.github.com>
2025-04-30 11:32:45 +0300

    Refactored hero health a few

--- src/DynastySurvivors/Assets/Code/Hero/HeroHealth.cs
+++ src/DynastySurvivors/Assets/Code/Hero/HeroHealth.cs
@@ -18,56 +18,59 @@ namespace Code.Hero
         private float _current;
         private float _max;
 
-        // public float Current => _current;
-        // public float Max => _max;
+        public float Current => _current;
+        public float Max => _max;
 
         public event Action Changed;
 
-        public float Current
-        {
-            get => _healthData.CurrentHealth;
-            set
-            {
-                if (_healthData.CurrentHealth != value)
-                {
-                    _healthData.CurrentHealth = value;
-        
-                    Changed?.Invoke();
-                }
-            }
-        }
-        
-        public float Max
-        {
-            get => _healthData.MaxHealth;
-            set => _healthData.MaxHealth = value;
-        }
+        // public float Current
+        // {
+        //     get => _healthData.CurrentHealth;
+        //     set
+        //     {
+        //         if (_healthData.CurrentHealth != value)
+        //         {
+        //             _healthData.CurrentHealth = value;
+        //
+        //             Changed?.Invoke();
+        //         }
+        //     }
+        // }
+        //
+        // public float Max
+        // {
+        //     get => _healthData.MaxHealth;
+        //     set => _healthData.MaxHealth = value;
+        // }
 
         public void LoadProgress(PlayerProgress progress)
         {
             _healthData = progress.HeroHealth;
+            
+            _current = _healthData.CurrentHealth;
+            _max = _healthData.MaxHealth;
             
             Changed?.Invoke();
         }
 
         public void UpdateProgress(PlayerProgress progress)
         {
-            progress.HeroHealth.CurrentHealth = Current;
-            progress.HeroHealth.MaxHealth = Max;
-            // progress.HeroHealth.CurrentHealth = _current;
-            // progress.HeroHealth.MaxHealth = _max;
+            // progress.HeroHealth.CurrentHealth = Current;
+            // progress.HeroHealth.MaxHealth = Max;
+            progress.HeroHealth.CurrentHealth = _current;
+            progress.HeroHealth.MaxHealth = _max;
         }
 
         public void TakeDamage(float damage)
         {
             float effectVisualDiration = 3f;
             
-            if (Current <= 0)
+            if (_current <= 0)
                 return;
 
-            Current -= damage;
+            _current -= damage;
             _animator.PlayHit();
-            // Changed?.Invoke();
+            Changed?.Invoke();
             
             GameObject damageEffect = Instantiate(_takeDamageEffectPrefab, transform.position, Quaternion.identity);
             Destroy(damageEffect, effectVisualDiration);

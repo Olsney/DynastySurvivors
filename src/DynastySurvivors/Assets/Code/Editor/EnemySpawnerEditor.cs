ef5e72461bc38fcdc9a1a819cb41373dc240de28
Olsney <104034829+Olsney@users.noreply.github.com>
2025-05-01 11:11:35 +0300

    Created enemy spawner and gizmos for him

---
+++ src/DynastySurvivors/Assets/Code/Editor/EnemySpawnerEditor.cs
@@ -0,0 +1,17 @@
+﻿using Code.Logic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(EnemySpawner))]
+    public class EnemySpawnerEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected)]
+        public static void RenderCustomGizmo(EnemySpawner enemySpawner, GizmoType gizmo)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(enemySpawner.transform.position, 0.5f);
+        }
+    }
+}
---
+++ src/DynastySurvivors/Assets/Code/Logic/EnemySpawner.cs
@@ -0,0 +1,14 @@
+﻿using Code.StaticData;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.Logic
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private EnemyTypeId _enemyTypeId;
+        [SerializeField] private bool _slain;
+        
+        private string _id;
+    }
+}

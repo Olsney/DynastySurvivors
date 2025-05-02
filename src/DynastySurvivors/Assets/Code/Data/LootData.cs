0d97a44de4799b32362cbd837a567d876cfb527a
Olsney <104034829+Olsney@users.noreply.github.com>
2025-05-03 00:13:58 +0300

    Created LootCounter

--- src/DynastySurvivors/Assets/Code/Data/LootData.cs
+++ src/DynastySurvivors/Assets/Code/Data/LootData.cs
@@ -6,10 +6,12 @@ namespace Code.Data
     public class LootData
     {
         public int Collected;
+        public Action Changed;
 
         public void Collect(Loot loot)
         {
             Collected += loot.Value;
+            Changed?.Invoke();
         }
     }
 }
---
+++ src/DynastySurvivors/Assets/Code/UI/LootCounter.cs
@@ -0,0 +1,28 @@
+﻿using Code.Data;
+using TMPro;
+using UnityEngine;
+
+namespace Code.UI
+{
+    public class LootCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _counter;
+        private WorldData _worldData;
+
+        public void Construct(WorldData worldData)
+        {
+            _worldData = worldData;
+            _worldData.LootData.Changed += UpdateCounter;
+        }
+
+        private void Start()
+        {
+            UpdateCounter();
+        }
+
+        private void UpdateCounter()
+        {
+            _counter.text = $"{_worldData.LootData.Collected}";
+        }
+    }
+}
